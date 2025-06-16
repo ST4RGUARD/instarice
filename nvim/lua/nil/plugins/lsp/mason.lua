9d81a721aa0d0493b8541adb5c1800847ea83a8d
ST4RGUARD <thema11ock@gmail.com>
2025-06-16 11:43:54 -0500

    Update mason.lua

--- nvim/lua/nil/plugins/lsp/mason.lua
+++ nvim/lua/nil/plugins/lsp/mason.lua
@@ -6,21 +6,12 @@ return {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "neovim/nvim-lspconfig",
-        -- "saghen/blink.cmp",
     },
     config = function()
-        -- import mason and mason_lspconfig
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
         local mason_tool_installer = require("mason-tool-installer")
 
-        -- NOTE: Moved these local imports below back to lspconfig.lua due to mason depracated handlers
-
-        -- local lspconfig = require("lspconfig")
-        -- local cmp_nvim_lsp = require("cmp_nvim_lsp")             -- import cmp-nvim-lsp plugin
-        -- local capabilities = cmp_nvim_lsp.default_capabilities() -- used to enable autocompletion (assign to every lsp server config)
-
-        -- enable mason and configure icons
         mason.setup({
             ui = {
                 icons = {
@@ -32,37 +23,45 @@ return {
         })
 
         mason_lspconfig.setup({
-            automatic_enable = false,
-            -- servers for mason to install
             ensure_installed = {
+                -- LSP servers only
                 "lua_ls",
-                -- "ts_ls", currently using a ts plugin
                 "html",
                 "cssls",
-                "gopls",
-                "emmet_ls",
-                "emmet_language_server",
+                "clangd",
+                "denols",
+                "jsonls",
                 "pyright",
-                "rubocop",
-                -- "eslint",
+                "ruff_lsp",
+                "rust_analyzer",
+                "emmet_ls",
                 "marksman",
+                "ruby_lsp",
             },
-
         })
 
         mason_tool_installer.setup({
             ensure_installed = {
-                "prettier", -- prettier formatter
-                "stylua",   -- lua formatter
-                "isort",    -- python formatter
-                "pylint",
-                "clangd",
-                "denols",
-                -- { 'eslint_d', version = '13.1.2' },
-            },
+                -- Formatters
+                "stylua",     -- Lua
+                "prettier",   -- JS/TS/HTML/CSS
+                "black",      -- Python
+                "isort",      -- Python
+                "rubocop",    -- Ruby
+                "clang-format", -- C/C++
 
-            -- NOTE: mason BREAKING Change! Removed setup_handlers
-            -- moved lsp configuration settings back into lspconfig.lua file
+                -- Linters
+                "pylint",     -- Python
+                "eslint_d",   -- JS/TS
+                "ruff",       -- Python (alternative linter)
+
+                -- Debuggers
+                "debugpy",    -- Python
+                "codelldb",   -- C/C++/Rust
+                "delve",      -- Go
+            },
+            auto_update = false,
+            run_on_start = true,
         })
     end,
 }
