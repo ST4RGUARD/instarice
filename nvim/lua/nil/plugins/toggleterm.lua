c6f7dec4d365d1fe234481d4fc1b43879794037c
ST4RGUARD <thema11ock@gmail.com>
2025-07-21 11:04:35 -0500

    adding floating term for quick commands

---
+++ nvim/lua/nil/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require("toggleterm").setup({
+      size = 10,           -- for horizontal or vertical
+      open_mapping = [[<C-\>]],
+      direction = "float", -- can be "horizontal", "vertical", "float", or "tab"
+      float_opts = {
+        width = 80,
+        height = 20,
+        border = "curved",
+        winblend = 0,
+      },
+      shade_terminals = true,
+      start_in_insert = true,
+      persist_size = true,
+    })
+
+    vim.keymap.set('n', '<leader>ts', '<cmd>ToggleTerm<cr>', { desc = 'Toggle floating terminal' })
+  end,
+}
